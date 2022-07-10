56e4342d2ea306d6043fb981e161f3a539071311
Cleber Barros <cleberbarrosgithub@gmail.com>
2022-07-10 14:53:41 -0300

    Criado repositorio para ativida desafio de projeto TDD aplicado a uma API

---
+++ src/main/java/one/digitalinnovation/beerstock/entity/Beer.java
@@ -0,0 +1,47 @@
+package one.digitalinnovation.beerstock.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.beerstock.mapper.enums.BeerType;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//Gera os métodos getters, setters, equals e hashcode
+@Data
+//Descreve a entidade do JPA, tabela nesse caso
+@Entity
+
+@NoArgsConstructor
+
+@AllArgsConstructor
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private int max;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BeerType type;
+
+
+}
